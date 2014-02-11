9b6d3cfbc7ca29ed4bda52add0dd499356e62543
sho kisaragi <sho@oshiire.to>
2014-02-11 20:09:05 +0900

    install postgresql-9.1 and create user & group for zabbix

--- site-cookbooks/postgresql/recipes/default.rb
+++ site-cookbooks/postgresql/recipes/default.rb
@@ -6,3 +6,55 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "postgresql-9.1" do
+  action :install
+end
+
+user "zabbix" do
+  uid 1011
+  home "/home/zabbix"
+  shell "/bin/bash"
+  password nil
+  supports :manage_home => true
+end
+
+group "zabbix" do
+  gid 1011
+  members ['zabbix']
+  action :create
+end
+
+service "postgresql" do
+  action [ :enable, :start ]
+  supports :status => true, :restart => true, :reload => true
+end
+
+template "postgresql.conf" do
+  path "/etc/postgresql/9.1/main/postgresql.conf"
+  source "postgresql.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode 0644
+  notifies :reload, 'service[postgresql]'
+end
+
+template "pg_hba.conf" do
+  path "/etc/postgresql/9.1/main/pg_hba.conf"
+  source "pg_hba.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode 0644
+  notifies :reload, 'service[postgresql]'
+end
+
+template "pg_ident.conf" do
+  path "/etc/postgresql/9.1/main/pg_ident.conf"
+  source "pg_ident.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode 0644
+  notifies :reload, 'service[postgresql]'
+end
+
+
