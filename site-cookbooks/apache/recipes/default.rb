2c3230181be213e317f229f143cecced55440f89
sho kisaragi <sho@oshiire.to>
2014-03-09 21:45:40 +0900

    Preparing to install wordpress on web server

--- site-cookbooks/apache/recipes/default.rb
+++ site-cookbooks/apache/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: apache
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, oshiire
 #
 # All rights reserved - Do Not Redistribute
 #
--- site-cookbooks/base/recipes/default.rb
+++ site-cookbooks/base/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: base
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, oshiire
 #
 # All rights reserved - Do Not Redistribute
 #
