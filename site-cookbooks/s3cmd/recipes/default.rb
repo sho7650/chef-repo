67a0f2faac98343e714597b1cb15814078387a42
sho kisaragi <sho@oshiire.to>
2014-11-22 20:06:32 +0900

    Stored access keys to encrypted data bag for s3cmd

--- site-cookbooks/s3cmd/recipes/default.rb
+++ site-cookbooks/s3cmd/recipes/default.rb
@@ -10,9 +10,20 @@ package "s3cmd" do
   action :install
 end
 
+data_bag = Chef::EncryptedDataBagItem.load('accesskey','aws')
+
+aws_access_key = data_bag['accesskey']
+aws_secret_key = data_bag['secretkey']
+aws_gpg_passphrase = data_bag['gpgpassphrase']
+
 template "s3cfg" do
   path  "/root/.s3cfg"
   owner "root"
   group "root"
   mode  0600
+  variables ({
+               :access_key => aws_access_key,
+               :secret_key => aws_secret_key,
+               :gpg_passphrase => aws_gpg_passphrase
+             })
 end
