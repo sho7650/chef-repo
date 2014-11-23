577dfff7b262cd7a7a4252dd7653367bdd8b864d
sho kisaragi <sho@oshiire.to>
2014-11-23 18:58:32 +0900

    Adding users for ldap

--- site-cookbooks/ldap/recipes/user.rb
+++ site-cookbooks/ldap/recipes/user.rb
@@ -12,6 +12,11 @@ accounts = data_bag('users')
 accounts.each do |id|
   users = data_bag_item('users', id)
 
+  execute  "add-#{users['uid']}" do
+    command "ldapadd -f #{Chef::Config[:file_cache_path]}/#{users['uid']}-user.ldif -x -D #{node[:ldap][:rootdn]} -w #{node[:ldap][:rootpw]}"
+    action :nothing
+  end
+
   template "#{users['uid']}-user.ldif" do
     source "user.ldif.erb"
     path   "#{Chef::Config[:file_cache_path]}/#{users['uid']}-user.ldif"
@@ -21,5 +26,6 @@ accounts.each do |id|
     variables ({
                  :u => users
                })
+    notifies :run, "execute[add-#{users['uid']}]"
   end
 end
